0f0a14d0b38824a2576a61bcdf363c123ff3e88e
Joris Vanhecke <joris@jorisvanhecke.be>
2021-05-15 19:32:03 +0200

    init repo

---
+++ lib/fluent/plugin/sentinel.rb
@@ -0,0 +1,10 @@
+require "fluent/plugin/sentinel/version"
+
+module Fluent
+  module Plugin
+    module Sentinel
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
